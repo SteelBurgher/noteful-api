0c80d494ea8a1912dd7439ba1addf2ab3f20e501
SteelBurgher <brandon.patrick.ellis@gmail.com>
2021-01-12 22:59:48 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://Sara@localhost/noteful',
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL: process.env.DB_URL || "postgresql://brandon@localhost/noteful",
+  TEST_DB_URL:
+    process.env.TEST_DB_URL || "postgresql://brandon@localhost/noteful",
+};
